9f69d4deb06136648059cbcd5f280c81a289f089
Tarini <tariniakanad@gmail.com>
2024-05-23 20:57:27 +0530

    Initial commit

---
+++ .venv/functions.py
@@ -0,0 +1,12 @@
+def get_todos(filepath='todo.txt'):
+    """" Read the text file and return the todo list """
+    with open(filepath, 'r') as file_lcl:
+        todos_lcl = file_lcl.readlines()
+    return todos_lcl
+
+
+def write_todos(todos_arg, filepath="todo.txt"):
+    """" Write todos in the text file """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
