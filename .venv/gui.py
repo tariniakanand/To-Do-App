d11bf605c24733f78377293cbe0ffc45e95584ae
Tarini <tariniakanad@gmail.com>
2024-05-24 02:25:10 +0530

    Gui commit

--- .venv/gui.py
+++ .venv/gui.py
@@ -4,6 +4,6 @@ import FreeSimpleGUI as sg
 label=sg.Text("Type a ToDo")
 input_box=sg.InputText(tooltip="Enter ToDo")
 add_button=sg.Button("Add")
-window=sg.Window("Tari's ToDo", layout=[[label], [input_box, add_button]])
+window=sg.Window("Tari's ToDo", layout=[[label, input_box, add_button]])
 window.read()
 window.close()
